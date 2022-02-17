f65e5e56e68f668b6ddc38a1fc2b2fd3d7bf32d5
niglelxrd <klementevdaniil@gmail.com>
2022-02-17 17:19:15 +0200

    StreamReader

--- Files/Program.cs
+++ Files/Program.cs
@@ -6,10 +6,12 @@ namespace Files
     class Program
     {
         const String Filename = "file.txt";
-
+        
         static void Main(string[] args)
         {
+            String path = @"C:\Users\user\source\repos\Week3\Files\bin\Debug\net6.0\loren.txt";
             Console.WriteLine("Hello Files!");
+            #region запись в файл
             try
             {
                 using (StreamWriter writer = new StreamWriter(Filename))
@@ -17,11 +19,34 @@ namespace Files
                     writer.WriteLine("Hello Files w/ Using() {}!");
 
                 }
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            
+            #endregion
+            #region чтение 
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("This file couldn't be read");
+                Console.WriteLine(ex.Message);
+            }
+           
+
+
+            #endregion
+
             Console.WriteLine("Done");
         }
     }
